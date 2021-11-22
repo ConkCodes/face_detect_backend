9e7d9b17560b8580af3131bd22ef2834208637bd
Connor <connoroakvillea32@hotmail.com>
2021-11-21 20:19:10 -0500

    worked on clean script and moved queries to the knex sql script folder

--- src/db/sql/scripts/deleteTables.js
+++ src/db/sql/scripts/deleteTables.js
@@ -1,7 +1,25 @@
 import db from "../../index.js";
 
-// DROP TABLE IF EXISTS users;
-db.schema.dropTableIfExists("users");
+const deleteTables = async () => {
+    try {
+        // DROP TABLE IF EXISTS users;
+        db.schema.hasTable("users").then(function(exists) {
+            if (exists) {
+                db.schema.dropTable("users");
+            }
+        });
+        // DROP TABLE IF EXISTS login;
+        db.schema.hasTable("login").then(function(exists) {
+            if (exists) {
+                db.schema.dropTable("login");
+            }
+        });
+        // success
+        console.log("all tables have been deleted");
+    // error
+    } catch (err) {
+        console.log("an error has occurred while deleting the tables");
+    }
+}
 
-// DROP TABLE IF EXISTS login;
-db.schema.dropTableIfExists("login");
+export default deleteTables;
--- src/scripts/clean.js
+++ src/scripts/clean.js
@@ -1,41 +1,18 @@
-import db from "../db/index.js";
+import deleteTables from "../db/sql/scripts/deleteTables.js";
+import createTables from "../db/sql/scripts/createTables.js";
 
 const clean = async () => {
-    // DROP TABLE IF EXISTS users;
-    //let exists = await db.schema.hasTable("users");
-    await db.schema.dropTable("users");
-    db.schema.hasTable("users").then(exists => {
-        if (exists) {
-            console.log("triggered");
-            db.schema.dropTable("users");
-        }
-    });
-    /*
-    if (exists) {
-        console.log("triggered");
-        await db.schema.dropTable("users");
-    }
-    */
-    // DROP TABLE IF EXISTS login;
-    if (await db.schema.hasTable("login")) db.schema.dropTable("login");
-    // CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(100), email VARCHAR(100) UNIQUE NOT NULL, entries BIGINT DEFAULT 0, joined TIMESTAMP NOT NULL);
-    if (!(await db.schema.hasTable("users"))) {
-        const usersTable = await db.schema.createTable("users");
-        usersTable.increments("id").primary();
-        usersTable.string("name", 100);
-        usersTable.string("email", 100).unique().notNullable();
-        usersTable.bigInteger("entries").defaultTo(0);
-        usersTable.timestamp("joined").defaultTo(db.fn.now()).notNullable();
-    }
-    // CREATE TABLE IF NOT EXISTS login (id SERIAL PRIMARY KEY, email VARCHAR(100) UNIQUE NOT NULL, hash VARCHAR(100) NOT NULL);
-    if (!(await db.schema.hasTable("login"))) {
-        const loginTable = await db.schema.createTable("login");
-        loginTable.increments("id").primary();
-        loginTable.string("email", 100).unique().notNullable();
-        loginTable.string("hash", 100).notNullable();
+    try {
+        // clean db
+        await deleteTables();
+        await createTables();
+        // success
+        console.log("the database has been cleaned");
+    // error
+    } catch (err) {
+        console.log("an error has occurred while cleaning the database");
     }
 }
 
 clean();
-console.log("the database has been cleaned");
-process.exit();
+// process.exit();
